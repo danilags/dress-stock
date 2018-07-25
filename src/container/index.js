582221ded6d42cdec5feed1f9d5df025f17f8c3a
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-07-26 04:09:46 +0700

    config routing and subroutes using react router dom

--- src/container/index.js
+++ src/container/index.js
@@ -1,37 +1,39 @@
-// import React from 'react';
-// import {
-//   BrowserRouter as Router,
-// } from 'react-router-dom'
+import React from 'react';
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom'
 
-// import PageApp from '../App';
-// import RouteWithSubRoutes from './Route';
+import MainApp from './MainApp';
+import HomePage from './HomePage';
+import DetailsPage from './DetailsPage';
+import RouteWithSubRoutes from './Route';
 
-// const routes = [
-//   {
-//     component: PageApp,
-//     routes: [
-//       {
-//         exact:true,
-//         path:'/',
-//         component: HomePage
-//       },     
-//       {
-//         exact:true,
-//         path:'/:id/:slug',
-//         component: MovieDetails
-//       },
-//     ]
-//   }
-// ];
+const routes = [
+  {
+    component: MainApp,
+    routes: [
+      {
+        exact:true,
+        path:'/',
+        component: HomePage
+      },     
+      {
+        exact:true,
+        path:'/products/:id/:slug',
+        component: DetailsPage
+      },
+    ]
+  }
+];
 
-// const AppRoute = () => (
-// 	<Router>
-// 		<div>
-// 			{routes.map((route, i) => (
-// 		 		<RouteWithSubRoutes key={i} {...route} text="Daniel Agus" />
-// 			))}
-// 		</div>
-// 	</Router>
-// );
+const AppRoute = () => (
+	<Router>
+		<div>
+			{routes.map((route, i) => (
+		 		<RouteWithSubRoutes key={i} {...route} text="Daniel Agus" />
+			))}
+		</div>
+	</Router>
+);
 
-// export default AppRoute;
+export default AppRoute;
