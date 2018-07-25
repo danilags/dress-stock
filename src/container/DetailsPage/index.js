84c9000f9935071784fa5c7e6505e16cb4bb01fa
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-07-26 04:09:58 +0700

    adding detail page

---
+++ src/container/DetailsPage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class DetailsPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>details page</h3>
+      </div>
+    )
+  }
+}
+
+export default DetailsPage;
