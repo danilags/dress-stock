38a79c13599da44870008ea88c2e3c8631b51078
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-07-29 00:16:57 +0700

    minor changes

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const port = 3001;
 
 server.use(middlewares)
 server.use(router)
-server.listen(3001, () => {
-  console.log('JSON Server is running')
+server.listen(port, () => {
+  console.log('JSON Server is running on port ' + port);
 })
